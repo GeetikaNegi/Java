ea098f267187a63c835d04cda82ad7ca7b7415cd
Geetika Negi <geetikanegi2601@gmail.com>
2023-07-11 21:23:39 +0530

    Commit

---
+++ Code/World.java
@@ -0,0 +1,21 @@
+class HelloWorld
+{
+	public static void main(String args[])
+	{
+		System.out.println("Class HelloWorld");
+	}
+}
+class New
+{
+	public static void main(String args[])
+	{
+		System.out.println("Class New");
+	}
+}
+class Old
+{
+	public static void main(String args[])
+	{
+		System.out.println("Class Old");
+	}
+}
